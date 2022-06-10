80bef3e1a172364f52b574a53c5f752db1cf9270
K.Tsukimoto <t.29.wuq@gmail.com>
2022-06-10 04:58:11 +0000

    Hello, Rust DPDK! EAL init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::os::raw::c_char;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Rust DPDK!");
+    let mut args: Vec<_> = env::args().map(|s| s.as_ptr() as *mut c_char).collect();
+    unsafe {
+        let ret = dpdk_sys::rte_eal_init(args.len() as i32, args.as_mut_ptr());
+        if ret < 0 {
+            panic!("Cannot init EAL\n");
+        }
+    }
 }
